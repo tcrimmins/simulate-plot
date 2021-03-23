801f0bc699d292cdf6e11f15ba0f48f14a8110f1
Theresa <theresa@usanpn.org>
2021-03-23 10:07:02 -0700

    initial commit with data simulation and plot

---
+++ simulate-plot-data.R
@@ -0,0 +1,11 @@
+# simulate and plot data - script from Github workshop w/Jeff Oliver
+# Theresa Crimmins
+# theresa@usanpn.org
+# 3-23-2021
+
+# simulate data from normal distribution
+x <- rnorm(n = 100)
+y <- 2 * x + rnorm(n = 100, sd = 0.1)
+
+# plot simulated data
+plot(x = x, y = y)
