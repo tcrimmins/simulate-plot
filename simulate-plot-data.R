9da24076b49ca220ca852c202aada0318fbfcb43
Theresa <theresa@usanpn.org>
2021-03-23 10:28:49 -0700

    increased noise in y value

--- simulate-plot-data.R
+++ simulate-plot-data.R
@@ -5,7 +5,7 @@
 
 # simulate data from normal distribution
 x <- rnorm(n = 100)
-y <- 2 * x + rnorm(n = 100, sd = 0.1)
+y <- 2 * x + rnorm(n = 100, sd = 0.2)
 
 # plot simulated data
 plot(x = x, y = y)
